1d07c8b9b93798c3a20f41883cb98f0a09e64ed7
staszewski <kamilstaszewski95@gmail.com>
2020-07-30 15:03:23 +0200

    remove clg

--- src/user-list.component.tsx
+++ src/user-list.component.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 
 export default function UserList() {
   const userList = useRecoilValue(users);
-  console.log(userList);
   return (
     <ul>
       {userList.map(({ id, name }: { id: number; name: string }) => (
